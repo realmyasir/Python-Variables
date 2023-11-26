e950b775ed7695d80066041ce809bd4fe511af9d
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-11-26 12:38:27 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 10:04:49 2023
+
+@author: Web 123
+"""
+
+
+#x=5
+#y=13
+#z=x+y
+#print(z)
+ 
+#1x=3
+#print=7
+#new_variable=9
+#s=x-y 
+#print(s)
+
+
+# a=2.5
+# b=3.14159
+# c=b*a**2
+# print(c)
+# radius=2.5
+# pi=3.14159
+# area=pi*radius**2
+# print(area)
+
+# pharse_1 = 'the cat sat on the mat !'
+# phrase_2 = 'and so did the Dog'
+# # phrase_3 = pharse_1 + ' ' +phrase_2
+
+# user_input = int(input('How may Apples do you have?\n>>>'))
+# help()
+
+# while=5
+
+
+
